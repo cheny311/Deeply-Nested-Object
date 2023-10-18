81645b6b6b8d6b2733b14ecc9a5d4ef8e2654380
Jessie Chen <112643063+cheny311@users.noreply.github.com>
2023-10-18 01:03:56 -0400

    javascript

---
+++ site.js
@@ -0,0 +1,71 @@
+//function created by chatgpt, after checking the current day, i want to have a fucntion that tells the user what kind of mood they mighe experience but i am still not sure after chatgpt generated the code
+var person ={
+    firstname: "Jessie",
+    lastname: "Chen",
+    age: 21,
+    menstrualCycle: [
+        'Menstrual', 'Menstrual', 'Menstrual', 'Follicular', 'Follicular',
+        'Follicular', 'Follicular', 'Ovulation', 'Ovulation', 'Ovulation',
+        'Ovulation', 'Luteal', 'Luteal', 'Luteal', 'Luteal', 'Luteal',
+        'Luteal', 'Luteal', 'Luteal', 'Luteal', 'Luteal', 'Luteal',
+        'Luteal', 'Luteal', 'Luteal', 'Luteal'
+    ],
+    menstrual: {
+        mood: ['okay mood', 'sad', 'Irritable'],
+        waterRetention: ['none'],
+        constipation: ['diaheara', 'flatulence'],
+        acnes: ['forehead','cheeks','jaw'],
+        cramp: true
+    },
+    luteal: {
+        mood: ['Low mood', 'Sad', 'Irritable'],
+        waterRetention: ['eyelids' , 'fingers' , 'butt'],
+        consitipation: ['gas', 'flatulence'],
+        acnes: ['forehead','cheeks','jaw'],
+        brainFog:['low producivities','dizziness'],
+        appetiteOverEating:true 
+    },
+    follicular: {
+        mood: ['calm', 'chill' ],
+        waterRetention: ['none'],
+        constipation: ['normal'],
+        acnes: ['few'],
+        brainFog:['none'],
+        cramp:false
+    },
+    ovulation: {
+        mood: ['happy', 'chill','outgoing' ],
+        waterRetention: ['none'],
+        constipation: ['normal'],
+        acnes: ['none'],
+        brainFog:['highproductivies'],
+        cramp:false
+    },
+    getCurrentStage: function(day) {
+        if (day >= 0 && day < this.menstrualCycle.length) {
+            return this.menstrualCycle[day];
+        } else {
+            return "Invalid day"; 
+        }
+    }
+}
+var currentDayInput = prompt("please enter a number from 0-27 to check your stage:");
+
+
+var currentDay = parseInt(currentDayInput);
+
+if (!isNaN(currentDay)) {
+    var currentStage = person.getCurrentStage(currentDay);
+    if (currentStage === "Invalid day") {
+        console.log("Invalid day");
+    } else {
+        console.log("Current stage: " + currentStage);
+    }
+} else {
+    console.log("Invalid day");
+}
+var currentDayInput = prompt("请输入一个数字作为当前日期：");
+var currentDay = parseInt(currentDayInput);
+
+
+
