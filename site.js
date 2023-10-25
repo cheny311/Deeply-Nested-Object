e556294ee2f37c01da79ad28ad021e622c5e59a7
Jessie Chen <112643063+cheny311@users.noreply.github.com>
2023-10-25 02:12:08 -0400

    test1

--- site.js
+++ site.js
@@ -54,17 +54,49 @@ let currentDayInput = prompt("please enter a number from 0-27 to check your stag
 
 let currentDay = parseInt(currentDayInput);
 
-if (!isNaN(currentDay)) {
-    let currentStage = person.getCurrentStage(currentDay);
-    if (currentStage === "Invalid day") {
-        console.log("Invalid day");
+
+function checkStage() {
+    let currentDayInput = document.getElementById("currentDayInput").value;
+    let currentDay = parseInt(currentDayInput);
+    let resultElement = document.getElementById("result");
+    let bodyElement = document.body;
+    let divElements = document.querySelectorAll(".menparent div"); 
+
+    if (!isNaN(currentDay)) {
+        let currentStage = person.getCurrentStage(currentDay);
+        resultElement.textContent = "Current stage: " + currentStage;
+
+        if (currentDay >= 0 && currentDay <= 2) {
+            bodyElement.style.backgroundColor = "green";
+            document.querySelectorAll(".mdiv1 img").forEach(img => img.style.display = "block");
+            document.querySelectorAll(".mdiv2 img").forEach(img => img.style.display = "block");
+        } else if (currentDay >= 3 && currentDay <= 7) {
+            bodyElement.style.backgroundColor = "red";
+            document.querySelectorAll(".mdiv1 img").forEach(img => img.style.display = "none");
+            document.querySelectorAll(".mdiv2 img").forEach(img => img.style.display = "none");
+        } else if (currentDay >= 8 && currentDay <= 11) {
+            bodyElement.style.backgroundColor = "blue";
+            document.querySelectorAll(".mdiv1 img").forEach(img => img.style.display = "none");
+            document.querySelectorAll(".mdiv2 img").forEach(img => img.style.display = "none");
+        } else if (currentDay >= 12 && currentDay <= 26) {
+            bodyElement.style.backgroundColor = "yellow";
+            document.querySelectorAll(".mdiv1 img").forEach(img => img.style.display = "none");
+            document.querySelectorAll(".mdiv2 img").forEach(img => img.style.display = "none");
+        }
+
+    
+        if (currentDay === 0) {
+            divElements[0].classList.add("mdiv1-custom");
+            divElements[1].classList.add("mdiv2-custom");
+        } else {
+            divElements[0].classList.remove("mdiv1-custom");
+            divElements[1].classList.remove("mdiv2-custom");
+        }
+        
     } else {
-        console.log("Current stage: " + currentStage);
+        resultElement.textContent = "Invalid day";
+        bodyElement.style.backgroundColor = null; 
     }
-} else {
-    console.log("Invalid day");
 }
-
-
 
 
